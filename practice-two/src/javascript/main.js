4c9a0a2f2f60912d429af3af01071061d96b71be
Nguyen Quang Huy <quanghuy1999@outlook.com.vn>
2024-04-24 05:25:57 +0700

    feat: filter notes by category

--- practice-two/src/javascript/main.js
+++ practice-two/src/javascript/main.js
@@ -1,4 +1,8 @@
 /* global document */
+
 import { renderAllNotes } from './modules/getNotes';
 
 document.addEventListener('DOMContentLoaded', renderAllNotes);
+
+import { renderNotesByCategory } from './modules/filterNotes';
+renderNotesByCategory;
