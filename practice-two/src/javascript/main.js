0effeb98598ba513b3afa76ac9150aeab9c7a747
Nguyen Quang Huy <quanghuy1999@outlook.com.vn>
2024-04-24 14:01:22 +0700

    fix: fix deploy failed

--- practice-two/src/javascript/main.js
+++ practice-two/src/javascript/main.js
@@ -1,9 +1,7 @@
 /* global document */
 import { renderAllNotes } from './modules/getNotes';
-import { addNotes } from './modules/addNotes"
+import { addNote } from './modules/addNote';
 
 document.addEventListener('DOMContentLoaded', renderAllNotes);
-
-
 
 addNote();
