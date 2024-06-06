fc326a52683a04d22cee14f7e807afd20240c9b1
Nguyen Quang Huy <quanghuy1999@outlook.com.vn>
2024-06-06 18:20:44 +0700

    fix: use form data

--- js-excercise/validate/index.js
+++ js-excercise/validate/index.js
@@ -1,13 +1,10 @@
 const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    validateInputs();
+    const formData = new FormData(form);
+    validateInputs(formData);
 });
 
 const setError = (element, message) => {
@@ -16,8 +13,8 @@ const setError = (element, message) => {
 
     errorDisplay.innerText = message;
     inputControl.classList.add('error');
-    inputControl.classList.remove('success')
-}
+    inputControl.classList.remove('success');
+};
 
 const setSuccess = element => {
     const inputControl = element.parentElement;
@@ -31,42 +28,43 @@ const setSuccess = element => {
 const isValidEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-}
+};
 
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+const validateInputs = formData => {
+    const formElements = form.elements;
+
+    const usernameValue = formData.get('username').trim();
+    const emailValue = formData.get('email').trim();
+    const passwordValue = formData.get('password').trim();
+    const password2Value = formData.get('password2').trim();
 
     if (usernameValue === '') {
-        setError(username, 'Username is required');
+        setError(formElements['username'], 'Username is required');
     } else {
-        setSuccess(username);
+        setSuccess(formElements['username']);
     }
 
     if (emailValue === '') {
-        setError(email, 'Email is required');
+        setError(formElements['email'], 'Email is required');
     } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
+        setError(formElements['email'], 'Provide a valid email address');
     } else {
-        setSuccess(email);
+        setSuccess(formElements['email']);
     }
 
     if (passwordValue === '') {
-        setError(password, 'Password is required');
+        setError(formElements['password'], 'Password is required');
     } else if (passwordValue.length < 8) {
-        setError(password, 'Password must be at least 8 character.')
+        setError(formElements['password'], 'Password must be at least 8 characters.');
     } else {
-        setSuccess(password);
+        setSuccess(formElements['password']);
     }
 
     if (password2Value === '') {
-        setError(password2, 'Please confirm your password');
+        setError(formElements['password2'], 'Please confirm your password');
     } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
+        setError(formElements['password2'], "Passwords don't match");
     } else {
-        setSuccess(password2);
+        setSuccess(formElements['password2']);
     }
-
 };
